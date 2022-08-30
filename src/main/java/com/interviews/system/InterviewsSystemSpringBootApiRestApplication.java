c08dc12fb28cb18fa6011124e2dba6854792fc54
Arturo Cossio <jazhiel12a@gmail.com>
2022-08-30 18:32:21 -0500

    stating-project

---
+++ src/main/java/com/interviews/system/InterviewsSystemSpringBootApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.interviews.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewsSystemSpringBootApiRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterviewsSystemSpringBootApiRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/interviews/system/InterviewsSystemSpringBootApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interviews.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewsSystemSpringBootApiRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
